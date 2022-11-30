6125e3efdd28bedd422fae58a41eba03a2375adb
wincess234 <66407164+wincess234@users.noreply.github.com>
2022-11-30 23:47:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,24 +2,24 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-def welcome():
-    return "Welcome All"
 
 def main():
-  st.title("Subtraction")
+  st.title("Division of Two Numbers")
   html_temp = """
-  <div style="background-color:tomato;padding:10px">
-  <h2 style="color:white;text-align:center;">Subtraction of 2 numbers using Streamlit</h2>
+  <div style="background-color:black;padding:10px">
+  <h2 style="color:black;text-align:center;">Division of 2 Numbers</h2>
   </div>
   """
   st.markdown(html_temp,unsafe_allow_html=True)
   num1 = st.number_input("Number 1")
   num2 = st.number_input("Number 2")
-  result = num1 - num2
+  
+  if(num1==0 or num2==0):
+    result = 0
+  else:
+    result=num1/num2
+  
   st.success('The output is {}'.format(result))
-  if st.button("Made By"):
-      st.text("Parth Bansal")
-      st.text("21f3000805")
-
+  
 if __name__=='__main__':
   main()
